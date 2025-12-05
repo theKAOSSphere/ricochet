94690fcd9d1ad766c45cfd00b2218df90338cd31
theKAOSSphere <thekaossphere@gmail.com>
2025-12-05 20:27:20 +0530

    Changed optimisation level in Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -3,7 +3,7 @@ CXX ?= g++
 
 # flags
 CXXFLAGS += -O3 -ffast-math -Wall -fPIC -DPIC $(shell pkg-config --cflags fftw3f) -I. -I../Shared_files
-LDFLAGS += -shared -Wl,-O1 -Wl,--as-needed -Wl,--no-undefined -Wl,--strip-all $(shell pkg-config --libs fftw3f) -larmadillo -lm
+LDFLAGS += -shared -Wl,-O3 -Wl,--as-needed -Wl,--no-undefined -Wl,--strip-all $(shell pkg-config --libs fftw3f) -larmadillo -lm
 
 ifneq ($(NOOPT),true)
 CXXFLAGS += -mtune=generic -msse -msse2 -mfpmath=sse
